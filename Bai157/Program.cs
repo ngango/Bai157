dc9091c6c304104c20c437fb3a1872dd7d339796
nga.ntt <nga.ntt@3si.vn>
2020-04-07 02:36:42 -0700

    code done 157

--- Bai157/Program.cs
+++ Bai157/Program.cs
@@ -8,10 +8,38 @@ namespace Bai157
 {
     class Program
     {
+        static IDictionary<int, int> d = new Dictionary<int, int>();
         static void Main(string[] args)
         {
-            var a = 1;
-            var b = 0;
+            int[] a = new int[] { 1, 2, 5,3, 4, 7,8, 9, 1, 7,8,9,9,10 };
+            XuatMang(a);
+        }
+        public static void XuatMang(int[] a)
+        {
+            int j = 0;
+            int m = 1;
+            for (int i=1;i< a.Length; i++)
+            {
+                if(a[i-1] >= a[i])
+                {
+                    d.Add(new KeyValuePair<int, int>(j, m));
+                    j = i;
+                    m = 1;
+                }
+                else
+                {
+                    m++;
+                }
+                if (i == a.Length - 1) d.Add(new KeyValuePair<int, int>(j, m));
+            }
+            int number = d.Max(x => x.Value);
+            var getArray = d.FirstOrDefault(x => x.Value == number);
+            for(int i=getArray.Key;i< number+ getArray.Key; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            Console.ReadKey();
+>>>>>>> code done 157
         }
     }
 }
